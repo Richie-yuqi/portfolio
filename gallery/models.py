d06720ee5303085c947cc0e2126b39f5ad0f4092
Unknown <yuqi.zuo@gmail.com>
2019-12-30 16:37:15 +0800

    完善Gallery App

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,15 @@ from django.db import models
 
 
 class Gallery(models.Model):
-	description = models.CharField(max_length=50) 
+	description = models.CharField(default='这里写描述',max_length=50) 
+	image = models.ImageField(default='default.exr',upload_to='image/') 
+	title = models.CharField(default='作品标题',max_length=50)
+    
+	def __str__(self):
+		return self.title 
+
+    
+
 
     
     
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
+    1. Add an import:  from my_app import viewsinconsistent use of tabs and spaces in indentation
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-]
-
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
